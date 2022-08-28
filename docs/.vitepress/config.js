fd3ecc9f8b97679adb491ac84cc2ab76365e4242
zuo <zuo@zmac.local>
2022-08-29 01:02:54 +0800

    feat(v0.1.0): 基础结构,页面更新

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+export default {
+  title: '左小白的经验积累',
+  description: '生活就像一个循环，时而勤奋，时而懒惰，反反复复。人一般比较健忘，在一个地方跌倒一次，后面大概率还会犯同一个错误，为了更好的管理时间、精力，避免重复犯同一个错误。这里记录每天的经验总结，沉淀、积累。',
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: '心态', link: '/mood/' },
+      { text: '美食', link: '/food/' },
+      { text: 'Github', link: 'https://github.com/zuoxiaobai' }
+    ],
+    sidebar: {
+      '/mood/': [
+        {
+          text: '心态',
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: 'Index', link: '/mood/' }, // /guide/index.md
+            { text: 'One', link: '/guide/one' }, // /guide/one.md
+            { text: 'Two', link: '/guide/two' } // /guide/two.md
+          ]
+        }
+      ],
+      '/food/': [
+        {
+          text: '美食',
+          items: [
+            // This shows `/guide/index.md` page.
+            { text: 'Index', link: '/food/' }, // /guide/index.md
+            { text: 'One', link: '/guide/one' }, // /guide/one.md
+            { text: 'Two', link: '/guide/two' } // /guide/two.md
+          ]
+        }
+      ]
+    }
+  }
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,10 @@
-# Hello VitePress
+---
+layout: home
+title: 首页
+---
+
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<Home />
