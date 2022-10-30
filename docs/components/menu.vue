6877ec490738e922538a968f01b912aebff97700
zuoxiaobai <guoqzuo@gmail.com>
2022-10-31 03:38:12 +0800

    更新菜谱 22-10-30

--- docs/components/menu.vue
+++ docs/components/menu.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ex-wrap">
     <div class="menu-title">
-      菜单食谱<span>（{{ total }}）</span>
+      菜单食谱<span>（{{ foodCount }}）</span>
     </div>
     <div class="ex-menu">
       <div
@@ -43,30 +43,36 @@
 <script lang="ts" setup>
 import { computed } from "vue";
 
+interface CategoryInfo {
+  name: string;
+  list: Array<{ name: string; path: string; todo?: boolean }>;
+}
 const vegetable = {
   name: "素菜",
   list: [
     { name: "青椒炒青豆", path: "#白菜猪肉炖粉条、青椒炒青豆-22-09-17" },
     { name: "清炒黄瓜", path: "#豆瓣罗非鱼、清炒黄瓜、番茄炒蛋-22-09-03" },
-    { name: "鱼香茄子", path: "#白灼虾、鱼香-小炒茄子-22-10-03" }, 
+    { name: "鱼香茄子", path: "#白灼虾、鱼香-小炒茄子-22-10-03" },
+    { name: "清炒冬瓜", path: "", todo: true },
     { name: "-", path: "豆制品" },
     { name: "尖椒豆腐皮", path: "#青椒回锅肉、尖椒豆腐皮-22-09-25" },
     { name: "香干炒肉", path: "#香干炒肉、白菜烧豆腐-22-09-18" },
     { name: "白菜烧豆腐", path: "#香干炒肉、白菜烧豆腐-22-09-18" },
     { name: "葱烧豆腐", path: "#葱烧豆腐、青椒肉丝-22-09-10" },
+    { name: "家常豆腐(嫩)", path: "#凉拌鸡胸肉、家常豆腐-嫩-22-10-30" },
+    { name: "麻婆豆腐", path: "", todo: true },
     { name: "-", path: "", desc: "根茎类" },
     { name: "青椒土豆片", path: "#豆豉鲮鱼油麦菜、青椒土豆片-22-09-12" },
     { name: "青椒土豆丝", path: "#青椒土豆丝、丝瓜炒蛋-22-08-27" },
+    { name: "清炒藕片", path: "", todo: true },
+    { name: "藕夹/藕盒", path: "", todo: true },
     { name: "-", path: "", desc: "鸡蛋类" },
     { name: "大葱炒鸡蛋", path: "#大葱炒鸡蛋、大葱炒鸡胸肉-22-09-11" },
     { name: "酱炒鸡蛋", path: "#酱炒鸡蛋-22-09-04" },
     { name: "西红柿炒鸡蛋", path: "#豆瓣罗非鱼、清炒黄瓜、番茄炒蛋-22-09-03" },
     { name: "丝瓜炒鸡蛋", path: "#青椒土豆丝、丝瓜炒蛋-22-08-27" },
     { name: "青椒炒鸡蛋", path: "#青椒鸡腿、青椒炒鸡蛋-22-08-28" },
-
-    { name: "", path: "" },
-    { name: "", path: "" },
-    { name: "", path: "" },
+    { name: "炸胡椒", path: "", todo: true }, // 湖北
   ],
 };
 
@@ -79,8 +85,10 @@ const greenVegetable = {
     { name: "蒜蓉上海青", path: "#香辣虾尾、蒜蓉上海青-22-10-02" },
     { name: "韭黄炒蛋", path: "#姜葱炒鸡、韭黄炒蛋-22-10-04" },
     { name: "清炒黄芽白菜", path: "#红烧鱼块、黄芽白菜-22-10-05" },
-    { name: "清炒空心菜", path: "", todo: true },
-    { name: "清炒红苋菜", path: "", todo: true },
+    { name: "清炒空心菜", path: "#土豆烧排骨、清炒空心菜-22-10-22" },
+    { name: "清炒红苋菜", path: "#清蒸鲈鱼、清炒红苋菜-22-10-29" },
+    { name: "蚝油生菜", path: "", todo: true },
+    { name: "手撕包菜", path: "", todo: true },
   ],
 };
 
@@ -91,8 +99,17 @@ const whiteMeat = {
     { name: "香煎鸡翅", path: "#香煎鸡翅、清炒小白菜-22-09-04" },
     { name: "青椒炒鸡腿", path: "#青椒鸡腿、青椒炒鸡蛋-22-08-28" },
     { name: "葱姜炒鸡", path: "#姜葱炒鸡、韭黄炒蛋-22-10-04" },
-    { name: "", path: "" },
-    { name: "", path: "" },
+    { name: "黄焖鸡", path: "", todo: true },
+    { name: "可乐鸡翅", path: "", todo: true },
+    { name: "葱油鸡", path: "", todo: true },
+    { name: "辣子鸡", path: "", todo: true },
+    { name: "新疆大盘鸡", path: "", todo: true },
+    { name: "三杯鸡", path: "", todo: true }, // https://www.bilibili.com/video/BV1dp4y1e73R
+    { name: "家常版酱鸭", path: "", todo: true }, 
+    { name: "家庭版咸水鸭", path: "", todo: true }, 
+    { name: "东名菜陈皮鸭", path: "", todo: true },  // https://www.bilibili.com/video/BV1we411L717
+    { name: "冰糖酸梅鸭", path: "", todo: true }, // https://www.bilibili.com/video/BV1Um4y1R7Na
+    { name: "砂锅啤酒鸭", path: "", todo: true }, // https://www.bilibili.com/video/BV1BP4y1M7w8
   ],
 };
 
@@ -102,11 +119,17 @@ const redMeat = {
     { name: "青椒回锅肉", path: "#青椒回锅肉、尖椒豆腐皮-22-09-25" },
     { name: "白菜猪肉炖粉条", path: "#白菜猪肉炖粉条、青椒炒青豆-22-09-17" },
     { name: "青椒肉丝", path: "#葱烧豆腐、青椒肉丝-22-09-10" },
-    { name: "土豆烧排骨", path: "", todo: true },
-    { name: "", path: "" },
-    { name: "", path: "" },
-    { name: "", path: "" },
-    { name: "", path: "" },
+    { name: "土豆烧排骨", path: "#土豆烧排骨、清炒空心菜-22-10-22" },
+    { name: "葱爆肉(大葱)", path: "", todo: true },
+    { name: "蒜苗回锅肉", path: "", todo: true },
+    { name: "蒜苔炒肉", path: "", todo: true },
+    { name: "猪脚饭", path: "", todo: true },
+    { name: "溜肉段", path: "", todo: true },
+    { name: "小酥肉", path: "", todo: true },
+    { name: "葱爆羊肉", path: "", todo: true },
+    { name: "酱牛肉", path: "", todo: true },
+    { name: "小炒黄牛肉", path: "", todo: true },
+    // { name: "", path: "", todo: true },
   ],
 };
 
@@ -118,29 +141,87 @@ const fish = {
     { name: "香辣虾尾", path: "#香辣虾尾、蒜蓉上海青-22-10-02" },
     { name: "白灼虾", path: "#白灼虾、鱼香-小炒茄子-22-10-03" },
     { name: "红烧鱼块", path: "#红烧鱼块、黄芽白菜-22-10-05" },
-    { name: "清蒸鲈鱼", path: "", todo: true },
-    { name: "", path: "" },
+    { name: "清蒸鲈鱼", path: "#清蒸鲈鱼、清炒红苋菜-22-10-29" },
+    { name: "炒虾球", path: "", todo: true },
+    { name: "红烧鲤鱼", path: "", todo: true },
+    { name: "香煎刁子鱼", path: "", todo: true },
+    { name: "红烧黄骨鱼", path: "", todo: true },
+    { name: "蒜香鲈鱼", path: "", todo: true },
+    { name: "油焖大虾", path: "", todo: true },
+    { name: "生炒田鸡", path: "", todo: true }, // https://www.bilibili.com/video/BV14Q4y1S7tZ
+    { name: "黄瓜烧鳝鱼", path: "", todo: true }, // https://www.bilibili.com/video/BV1MF411V7GJ/
+    { name: "藿香鳝鱼", path: "", todo: true }, //  https://www.bilibili.com/video/BV1ut411P7HM/
+    { name: "泥鳅烧黄瓜", path: "", todo: true }, // https://www.bilibili.com/video/BV1hD4y1y7Kv
+    { name: "干煸泥鳅", path: "", todo: true }, // https://www.bilibili.com/video/BV1434y147Rp
   ],
 };
 
 const soup = {
   name: "汤",
-  list: [{ name: "", path: "" }],
+  list: [
+    { name: "鲫鱼豆腐汤", path: "", todo: true },
+    { name: "莲藕排骨汤", path: "", todo: true },
+    { name: "-", path: "", desc: "素菜类" },
+    { name: "丝瓜鸡蛋汤", path: "", todo: true },
+    { name: "西红柿鸡蛋汤", path: "", todo: true },
+    { name: "平菇鸡蛋汤", path: "", todo: true },
+    { name: "紫菜蛋花汤", path: "", todo: true },
+  ],
 };
 
 const coldDish = {
   name: "凉菜",
-  list: [{ name: "", path: "" }],
+  list: [
+    { name: "凉拌鸡胸肉", path: "#凉拌鸡胸肉、家常豆腐-嫩-22-10-30" },
+    { name: "口水鸡", path: "", todo: true },
+    { name: "手撕鸡", path: "", todo: true },
+    { name: "白切鸡", path: "", todo: true }, // https://www.bilibili.com/video/BV1se4y1a7C5
+    { name: "鲜椒杏鲍菇", path: "", todo: true },
+    { name: "拍黄瓜", path: "", todo: true },
+  ],
 };
 
-const total = computed(() => {
+const luDish = {
+  name: "卤菜",
+  list: [
+    { name: "卤藕片", path: "", todo: true },
+    { name: "卤千张/豆腐皮", path: "", todo: true },
+    { name: "卤海带", path: "", todo: true },
+    { name: "卤土豆片", path: "", todo: true },
+    { name: "卤蛋", path: "", todo: true },
+    { name: "卤肉", path: "", todo: true },
+    { name: "卤鸡", path: "", todo: true },
+  ],
+};
+
+const huoguo = {
+  name: "火锅",
+  list: [
+    { name: "牛油锅底", path: "", todo: true },
+    { name: "番茄锅底", path: "", todo: true },
+  ]
+}
+
+const zheng = {
+  name: "蒸菜",
+  list: [
+    { name: "蒸菜(藜蒿)", path: "", todo: true },
+    { name: "粉蒸肉", path: "", todo: true },
+    { name: "粉蒸藕块", path: "", todo: true },
+    { name: "粉蒸土豆块", path: "", todo: true },
+  ]
+}
+
+const foodCount = computed(() => {
   let num = 0;
-  menuList.forEach((category) => {
+  let todo = 0;
+  menuList.forEach((category: CategoryInfo) => {
     num += category.list.filter(
       (item) => item.name && item.name !== "-"
     ).length;
+    todo += category.list.filter((item) => item?.todo).length;
   });
-  return num;
+  return `${num - todo}/${num}`
 });
 
 const menuList = [
@@ -151,6 +232,9 @@ const menuList = [
   fish,
   soup,
   coldDish,
+  luDish,
+  huoguo,
+  zheng
 ];
 </script>
 
